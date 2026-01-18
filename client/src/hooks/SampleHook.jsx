ffa6ffd854cadb8618427cb70001c9691d67bafc
tyrusr <tyrusr17@yahoo.com>
2026-01-17 20:27:17 -0800

    made a simple hook layout

---
+++ client/src/hooks/SampleHook.jsx
@@ -0,0 +1,14 @@
+//get dependancies
+
+//useeffect
+//state maybe here
+//arrow function
+//setloading true
+//try to get data from back end by calling services
+//setstate if good
+//set error if error
+//finally set loading false
+
+//call arrof function
+
+//[]
