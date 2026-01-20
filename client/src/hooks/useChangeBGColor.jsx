a43d72bd052b40053a79183b733a430f241cbd1d
tyrusr <tyrusr17@yahoo.com>
2026-01-19 20:57:29 -0800

    made change bg color hook file and psudo code layout

---
+++ client/src/hooks/useChangeBGColor.jsx
@@ -0,0 +1,15 @@
+//import dependancies
+
+//make a use effect
+
+//make states or use context
+
+//async function
+
+//grab the current display color from dom or maybe pass it to hook as a prop
+
+//update state/context with new color
+
+//call async function
+
+//make [] thing work with when the button is touched
