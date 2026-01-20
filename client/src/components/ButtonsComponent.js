7c9ea04604cbe55c7a50961026e85fbc091a6a3b
tyrusr <tyrusr17@yahoo.com>
2026-01-19 20:52:30 -0800

    made button component and psudo code layout for it

---
+++ client/src/components/ButtonsComponent.js
@@ -0,0 +1,8 @@
+//import dependancies
+
+//make a function
+//make the event handler for each button or once for all
+//make a return
+    //make a h1
+    //make the three buttons
+//export if not export default
