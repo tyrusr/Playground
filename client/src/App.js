f823551de1a67a0307dd83a87989f772a8f911c3
tyrusr <tyrusr17@yahoo.com>
2026-01-20 21:44:00 -0800

    imported alert button into app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import { AlertButton } from "./components/ButtonsComponentAlert"
 
 function App() {
   return (
     <div className="App">
+      <AlertButton />
     </div>
   );
 }
