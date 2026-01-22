ce6d4e7b02365bf944f38b2f105c09bad400727e
tyrusr <tyrusr17@yahoo.com>
2026-01-22 13:32:18 -0800

    did some hook reps inside useRepititionHooks

--- client/src/hooks/useRepetitionHooks.jsx
+++ client/src/hooks/useRepetitionHooks.jsx
@@ -2,3 +2,93 @@
 
 import { useState } from "react";
 
+export default function useExample1() {
+    const [value, setValue] = useState(null);
+
+    return {
+        value,
+        setValue
+    };
+}
+
+export default function useExample2() {
+    const[value, setValue] = useState(null);
+
+    return {
+        value,
+        setValue
+    }
+}
+
+export default function useExample3() {
+    cosnt [value, setValue] = useState(null);
+
+    return {
+        value,
+        setValue
+    }
+}
+
+export default function useExample4() {
+    const [value, setValue] = useState(null);
+
+    return {
+        value,
+        setValue
+    };
+}
+
+export default function useExample5() {
+    const [value, setValue] = useState(null);
+
+    return {
+        value,
+        setValue
+    };
+}
+
+export default function useExample6() {
+    const [value, setValue] = useState(null);
+
+    return {
+        value,
+        setValue
+    };
+}
+
+export default function useExample7() {
+    cosnt [value, setValue] = useState(null);
+
+    return {
+        value,
+        setValue
+    };
+}
+
+export function useExample8() {
+    const [value, setValue] = useState(null);
+
+    return {
+        value,
+        setValue
+    };
+}
+
+export function useExample9() {
+    const [value, setValue] = useState(null);
+
+    return {
+        value,
+        setValue
+    };
+}
+
+export function useExample10() {
+    const [value, setValue] = useState(null);
+
+    return {
+        value,
+        setValue
+    };
+}
+
