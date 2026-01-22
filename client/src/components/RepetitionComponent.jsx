5acb7c2b73fa8e8826504836bebfa7c1fcddc211
tyrusr <tyrusr17@yahoo.com>
2026-01-21 18:56:53 -0800

    did some more component reps

--- client/src/components/RepetitionComponent.jsx
+++ client/src/components/RepetitionComponent.jsx
@@ -1,2 +1,63 @@
 // for getting multiple practice reps in
 
+function Example1() {
+    return (
+        <h1>rep1</h1>
+    );
+}
+
+export default Example1;
+
+export function Example2() {
+    return (
+        <h1>rep2</h1>
+    );
+}
+
+export default function Example3() {
+    return (
+        <h1>rep3</h1>
+    );
+}
+
+export default function Example4() {
+    return (
+        <h1>rep4</h1>
+    );
+}
+
+export default function Example5() {
+    return (
+        <h1>rep5</h1>
+    );
+}
+
+export default function Example6() {
+    return (
+        <h1>rep6</h1>
+    );
+}
+
+export default function Example7() {
+    return (
+        <h1>rep7</h1>
+    );
+}
+
+export default function Example8() {
+    return (
+        <h1>rep8</h1>
+    );
+}
+
+export default function Example9() {
+    return (
+        <h1>rep9</h1>
+    );
+}
+
+export default function Example10() {
+    return (
+        <h1>rep10</h1>
+    );
+}
