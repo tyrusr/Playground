30f29647c22a193418f8eed6867576aa39120c82
tyrusr <tyrusr17@yahoo.com>
2026-01-23 02:45:24 -0800

    did some more reps in userepitition hook but more advanced

--- client/src/hooks/useRepetitionHooks.jsx
+++ client/src/hooks/useRepetitionHooks.jsx
@@ -1,3 +1,87 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 // for getting multiple practice reps in
 
+export default function useRep1() {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState();
+
+    useEffect(() => {
+        setLoading(true);
+        const getData = async () => {
+            try {
+                const res = await sampleservicescall();
+                setData(res);
+            } catch(err) {
+                setError(err);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        getData();
+    }, []);
+
+    return {
+        data,
+        loading,
+        error,
+        getData
+    }
+}
+
+export function useRep2() {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState();
+
+    useEffect(() => {
+        setLoading(true);
+        const getData = async () => {
+            try {
+                const res = await sampleservicescall();
+                setData(res);
+            } catch(err) {
+                setError(err);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        getData();
+    }, []);
+
+    return {
+        data,
+        loading,
+        error,
+        getData
+    }
+}
+
+const useRep1 = () => {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState();
+
+    useEffect(() => {
+        setLoading(true);
+        const getData = async () => {
+            try {
+                const res = await sampleservicescall();
+                setData(res);
+            } catch(err) {
+                setError(err);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        getData();
+    }, []);
+
+    return {
+        data, error, loading, getData
+    }
+}
+ 
