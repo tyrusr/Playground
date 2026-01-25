920c47c308e234f747f7076d2cd459fc4c872376
tyrusr <tyrusr17@yahoo.com>
2026-01-25 00:49:57 -0800

    did some reps inside repititionservices

--- client/src/services/repititionServices.jsx
+++ client/src/services/repititionServices.jsx
@@ -1,1 +1,127 @@
 // for getting multiple practice reps in
+
+export async function Example1(parameters) {
+    const csrfToken = "import from cookies";
+
+    try{
+        const response = await fetch('/route', {
+            method: "POST",
+            headers:{
+                "Content-Type": "application/json",
+                "X-CSRF-Token": csrfToken,
+            },
+            credentials: 'include',
+
+            body: JSON.stringify({parameters})
+        });
+
+        if (!response.ok) throw new Error("response was not ok");
+
+        const res = await response.json();
+
+        return res;
+    } catch(err) {
+        throw new Error(err);
+    }
+};
+
+export async function Example1(parameters) {
+    const csrfToken = "import from cookies";
+
+    try {
+        const response = await fetch('/route',{
+            method: 'POST',
+            headers:{
+                "Content-Type": "application/json",
+                "X-CSRF-Token": csrfToken,
+            }, 
+            credentials: "include",
+            body: JSON.stringify({parameters})
+        });
+
+        if (!response.ok) throw new Error("response was not ok");
+
+        const res = await response.json();
+
+        return res;
+    } catch(err) {
+        throw new Error(err);
+    }
+};
+
+export async function Example3(params) {
+    const csrfToken = "get token from cookies";
+
+    try{
+        const response = await fetch('/route', {
+            method: "POST",
+            headers:{
+                "Content-Type": "application/json",
+                "X-CSRF-Token": csrfToken,
+            },
+            credentials: "include",
+
+            body: JSON.stringify({params})
+        })
+
+        if(!response.ok) throw new Error ("response was not ok");
+
+        const res = await response.json();
+
+        return res;
+    } catch(err) {
+        throw new Error (err);
+    }
+};
+
+export async function Example4(params) {
+    const csrfToken = "get from cookies";
+
+    try {
+        const response = await fetch('/route', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "X-CSRF-Token": csrfToken,
+            },
+            credentials: "include",
+
+            body: JSON.stringify({params})
+        });
+
+        if (!response.ok) throw new Error ("response not ok");
+
+        const res = await response.json();
+
+        return res;
+    } catch(err) {
+        throw new Error(err);
+    }
+};
+
+const Example5 = async (params) => {
+    const csrfToken = "get from cookies";
+
+    try {
+       const response = await fetch("/route", {
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json",
+            "X-CSRF-Token": csrfToken,
+        },
+        credentials: "include",
+
+        body: JSON.stringify({params})
+       });
+       
+       if (!response.ok) {
+        throw new Error ("response was not ok")
+       };
+
+       const res = response.json();
+
+       return res;
+    } catch(err) {
+        throw new Error (err);
+    }
+};
