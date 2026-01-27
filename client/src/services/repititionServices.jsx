1fa78c5deb55b7b73978fad445b6d6952f27c1f8
tyrusr <tyrusr17@yahoo.com>
2026-01-26 20:26:05 -0800

    did one rep of each to test memory

--- client/src/services/repititionServices.jsx
+++ client/src/services/repititionServices.jsx
@@ -1,2 +1,26 @@
 // for getting multiple practice reps in
 
+export default async function getData(params){
+    const csrfToken = "get from cookies";
+
+    try {
+        const response = await fetch('route', {
+            method:"POST",
+            headers: {
+                "Content-Type": "application/json",
+                "X-CSRF-Token": csrfToken,
+            },
+            credentials: "include",
+
+            body: JSON.stringify({params})
+        });
+
+        if (!response.ok) throw new Error ("response was not ok");
+
+        const res = await response.json();
+
+        return res;
+    } catch(err) {
+        throw new Error(err);
+    }
+};
