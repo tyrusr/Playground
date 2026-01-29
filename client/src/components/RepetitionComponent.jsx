ea7630b4d95ae3f2df07b0b81ea95499f9c9c5fa
tyrusr <tyrusr17@yahoo.com>
2026-01-28 18:42:39 -0800

    did some reps in repitition component

--- client/src/components/RepetitionComponent.jsx
+++ client/src/components/RepetitionComponent.jsx
@@ -1,1 +1,74 @@
 // for getting multiple practice reps in
+
+export default function Example1() {
+    return (
+        <h1>rep1</h1>
+    );
+};
+
+export function Example2() {
+    return (
+        <h1>rep2</h1>
+    );
+};
+
+const Example3 = () => {
+    return (
+        <h1>rep3</h1>
+    );
+};
+
+export Example3;
+
+export default function Example4() {
+    return (
+        //button with event handler can go here, call custum imported hook on click
+        <h1>rep3</h1>
+    );
+};
+
+export function Example4() {
+    //function to handle click
+        //call hook and pass any params
+
+    return (
+        //button with event handler can go here, call function to handle click
+        <h1>rep4</h1>
+    );
+};
+
+export default function Example5() {
+    return (
+        <h1>rep5</h1>
+    );
+};
+
+export function Example6() {
+    return (
+        <h1>rep6</h1>
+    );
+};
+
+export function Example7() {
+    return (
+        <h1>rep7</h1>
+    );
+};
+
+export function Example8() {
+    return (
+        <h1>rep8</h1>
+    );
+};
+
+export function Example9() {
+    return (
+        <h1>rep9</h1>
+    );
+};
+
+export function Example10() {
+    return (
+        <h1>rep10</h1>
+    );
+};
