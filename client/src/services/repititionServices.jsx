a9882ecfec76d1672a46acf0c767110c38ff0c94
tyrusr <tyrusr17@yahoo.com>
2026-01-30 19:37:11 -0800

    cleaned up repititionservices

--- client/src/services/repititionServices.jsx
+++ client/src/services/repititionServices.jsx
@@ -1,24 +1,2 @@
 // for getting multiple practice reps in
 
-export default function Example1(params) {
-    const csrfToken = "get from cookies";
-
-    try {
-        const response = fetch('/route', {
-            method:"POST",
-            headers: {
-                "Content-Type": "application/json",
-                "X-CSRF-Token": csrfToken,
-            },
-            credentials: "include",
-
-            body: JSON.stringify(params)
-        });
-
-        if (!response.ok) throw new Error("response was not ok");
-
-        return response;
-    } catch(err) {
-        throw new Error(err);
-    }
-};
