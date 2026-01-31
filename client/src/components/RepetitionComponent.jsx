e1d867a772ca51fd2c6c7393a578bc71faf4bfdb
tyrusr <tyrusr17@yahoo.com>
2026-01-30 18:07:36 -0800

    did some warm up reps for components, hooks and services

--- client/src/components/RepetitionComponent.jsx
+++ client/src/components/RepetitionComponent.jsx
@@ -1,1 +1,7 @@
 // for getting multiple practice reps in
+
+export function Example1() {
+    return (
+        <h1>rep1</h1>
+    );
+};
--- client/src/hooks/useRepetitionHooks.jsx
+++ client/src/hooks/useRepetitionHooks.jsx
@@ -1,3 +1,26 @@
 import { useEffect, useState } from "react";
 // for getting multiple practice reps in
 
+export default function useExample1() {
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        async function getData() {
+            setLoading(true);
+
+            try {
+                const res = await sampleservicescall();
+
+                setData(res);
+            } catch(err) {
+                setError(err);
+            } finally {
+                setLoading(false);
+            }
+        };
+    }, []);
+
+    return {data, error, loading};
+};
