baef18b8f8e0b7b896283b1bec816397d1fe6c87
tyrusr <tyrusr17@yahoo.com>
2026-02-04 11:55:19 -0800

    did warmup rep in repitition controller

--- server/controllers/repititionController.js
+++ server/controllers/repititionController.js
@@ -1,2 +1,13 @@
 // for getting multiple practice reps in
 
+export async function Example1(req, res, next) {
+    const params = req.body;
+
+    try {
+        // logic goes here
+
+        res.status().json();
+    } catch(err) {
+        next(err);
+    }
+}
