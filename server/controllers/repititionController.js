84cf5a7a18ab5553ce27d413892cd20415c5c632
tyrusr <tyrusr17@yahoo.com>
2026-02-04 01:57:49 -0800

    cleaned up repitition controler

--- server/controllers/repititionController.js
+++ server/controllers/repititionController.js
@@ -1,29 +1,2 @@
 // for getting multiple practice reps in
 
-export async function Example1(req, res, next){
-    const content = req.body;
-
-    try {
-        //logic goes here
-
-        res.status().json();
-    } catch(err){
-        next(err);
-    }
-}
-
-export async function Example2(req, res, next){
-    const content = req.body;
-
-    try {
-        // logic goes here
-
-        res.status().json();
-    } catch(err) {
-        //if username didnt match return a specific error code
-
-        //if username didnt exist return a specific error code/message
-
-        next(err);
-    }
-}
