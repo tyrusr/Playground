6f40b76e4e11240508b3fce600c453e5d31ddfb1
tyrusr <tyrusr17@yahoo.com>
2026-02-04 12:12:25 -0800

    did a warmup in userepititionhooks

--- client/src/hooks/useRepetitionHooks.jsx
+++ client/src/hooks/useRepetitionHooks.jsx
@@ -1,2 +1,28 @@
 import { useEffect, useState } from "react";
 // for getting multiple practice reps in
+
+export default function useGetData(params) {
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading]= useState(false);
+
+    useEffect(() => {
+        async function getData() {
+            setLoading(true);
+
+            try {
+                const response = await sampleservicescall();
+
+                setData(response);
+            } catch(err) {
+                setError(err);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        getData();
+    }, []);
+
+    return {data, error, loading};
+}
