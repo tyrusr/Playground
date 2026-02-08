2ec529a33169be5ab7c9eebae6603be75affec0a
tyrusr <tyrusr17@yahoo.com>
2026-02-07 20:50:17 -0800

    did a warmup in repitition controller

--- server/controllers/repititionController.js
+++ server/controllers/repititionController.js
@@ -1,1 +1,13 @@
 // for getting multiple practice reps in
+
+export async function Example1(req, res, next) {
+    const request = req.data;
+
+    try {
+        //logic
+
+        return res.status().json();
+    } catch(err) {
+        return res.status().json();
+    }
+}
