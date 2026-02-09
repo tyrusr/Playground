272da21f37edcfdcabb46e642903b992033f186a
tyrusr <tyrusr17@yahoo.com>
2026-02-09 14:00:43 -0800

    did some psudo code layout in practice button

---
+++ client/src/components/PracticeButton.jsx
@@ -0,0 +1,9 @@
+
+
+
+
+//make a component
+//deconstructor that gets the stuff from hook
+//event handler that calls hook and then sends an alert when response
+//button in component
+//event listener in button
