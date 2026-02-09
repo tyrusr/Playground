a114cf79dce991e14f681ffaebf6394672ad2b4f
tyrusr <tyrusr17@yahoo.com>
2026-02-08 18:10:41 -0800

    did a warmup in services

--- client/src/services/repititionServices.jsx
+++ client/src/services/repititionServices.jsx
@@ -1,2 +1,27 @@
 // for getting multiple practice reps in
 
+export default async function Rep1(params) {
+    const csrfToken = "get from cookies";
+
+    try {
+        const response = await fetch('/route', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "X-CSRF-Token": csrfToken,
+            },
+            credentials: "include",
+
+            body: JSON.stringify({params})
+        });
+
+        if (!response.ok) throw new Error("response not ok");
+
+        const res = await response.json();
+
+        return res;
+
+    } catch(err) {
+        throw new Error(err);
+    }
+}
