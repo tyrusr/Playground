2e3176ac9007964d9ad3bcfa695c8595be740ad3
tyrusr <tyrusr17@yahoo.com>
2026-02-08 17:55:50 -0800

    did a warmpup component

--- client/src/components/RepetitionComponent.jsx
+++ client/src/components/RepetitionComponent.jsx
@@ -1,1 +1,7 @@
 // for getting multiple practice reps in
+
+export default function Rep1() {
+    return (
+        <h1>rep1</h1>
+    );
+}
--- server/controllers/repititionController.js
+++ server/controllers/repititionController.js
@@ -1,13 +1,2 @@
 // for getting multiple practice reps in
 
-export async function Example1(req, res, next) {
-    const request = req.data;
-
-    try {
-        //logic
-
-        return res.status().json();
-    } catch(err) {
-        return res.status().json();
-    }
-}
