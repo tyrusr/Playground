4e30c562d796fe5f8f975434b6b4c42d91824ffd
tyrusr <tyrusr17@yahoo.com>
2026-02-09 13:25:25 -0800

    cleaned up repitition controller

--- server/controllers/repititionController.js
+++ server/controllers/repititionController.js
@@ -1,13 +1,2 @@
 // for getting multiple practice reps in
 
-export async function Rep1(req, res, next){
-    const data = req.body;
-
-    try {
-        //logic goes here
-
-        res.response().json();
-    } catch(err) {
-        next(err);
-    }
-}
