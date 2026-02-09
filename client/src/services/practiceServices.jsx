eb149dbb64be715e235e74611c5e980bce046864
tyrusr <tyrusr17@yahoo.com>
2026-02-09 14:07:25 -0800

    did some psudo code layout in practice services

---
+++ client/src/services/practiceServices.jsx
@@ -0,0 +1,11 @@
+
+//make services function
+//variable that equals await fetch
+//method get
+//headers
+//credentials include
+
+//if variable not ok throw error
+
+//else return variable
+//catch error
