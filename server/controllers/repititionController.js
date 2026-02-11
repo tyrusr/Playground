7d675591ea7ee75de0b40d0f685f103790060fa3
tyrusr <tyrusr17@yahoo.com>
2026-02-10 23:05:06 -0800

    did some reps in repitition controller

--- server/controllers/repititionController.js
+++ server/controllers/repititionController.js
@@ -1,2 +1,13 @@
 // for getting multiple practice reps in
 
+export default function rep1(req, res, next) {
+    const content = req.body;
+
+    try {
+        //logic goes here
+
+        res.status(200).json()
+    } catch(err) {
+        next(err);
+    }
+}
