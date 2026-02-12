15f204d12a08689b346179a7e333beeb8f7bc065
tyrusr <tyrusr17@yahoo.com>
2026-02-12 14:02:12 -0800

    did soem reps in repitition controller

--- server/controllers/repititionController.js
+++ server/controllers/repititionController.js
@@ -1,2 +1,121 @@
 // for getting multiple practice reps in
 
+export async function Rep1(req, res, next) {
+    const request = req.body;
+
+    try {
+        //logic goes here
+
+        res.status().json();
+    } catch(err) {
+        next(err);
+    }
+}
+
+export async function Rep2(req, res, next) {
+    const request = req.body;
+
+    try {
+        //logic goes here
+
+        res.status().json();
+    } catch(err) {
+        next(err);
+    }
+}
+
+export async function Rep3(req, res, next) {
+    const request = req.body;
+
+    try {
+        //logic goes here
+
+        res.status().json();
+    } catch(err) {
+        next(err);
+    }
+}
+
+export async function Rep4(req, res, next) {
+    const request = req.body;
+
+    try {
+        //logic
+
+        res.status().json();
+    } catch(err) {
+        next(err);
+    }
+}
+
+export async function Rep5(req, res, next) {
+    const request = req.body;
+
+    try {
+        //logic goes here
+
+        res.status().json();
+    } catch(err) {
+        next(err);
+    }
+}
+
+export async function Rep6(req, res, next) {
+    const request = req.body;
+
+    try {
+        //logic goes here
+
+        res.status().json();
+    } catch(err) {
+        next(err);
+    }
+}
+
+export async function Rep7(req, res, next) {
+    const request = req.body;
+
+    try {
+        //logic
+
+        res.status().json();
+    } catch(err) {
+        next(err);
+    }
+}
+
+export async function Rep8(req, res, next) {
+    const request = req.body;
+
+    try {
+        //logic
+
+        res.status().json();
+    } catch(err) {
+        next(err);
+    }
+}
+
+export async function Rep9(req, res, next) {
+    const request = req.body;
+
+    try {
+        //logic 
+
+        res.status().json();
+    } catch(err) {
+        next(err);
+    }
+}
+
+export async function Rep10(req, res, next) {
+    const request = req.body;
+
+    try {
+        //logic
+
+        res.status().json();
+    } catch(err) {
+        next(err);
+    }
+}
