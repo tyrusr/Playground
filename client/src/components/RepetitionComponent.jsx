b96781172146ddbe7c25ebe57d36358750c69295
tyrusr <tyrusr17@yahoo.com>
2026-02-21 01:49:16 -0800

    did some reps in repitition component

--- client/src/components/RepetitionComponent.jsx
+++ client/src/components/RepetitionComponent.jsx
@@ -19,3 +19,55 @@ const Rep3 = () => {
 }
 
 export Rep3;
+
+export default function Rep4() {
+    return (
+        <h1>rep4</h1>
+    );
+}
+
+export function Rep4 () {
+    return (
+        <h1>rep4</h1>
+    );
+}
+
+const Rep5 = () => {
+    return (
+        <h1>rep5</h1>
+    );
+}
+
+export { Rep5 };
+
+export default function Rep6() {
+    return (
+        <h1>rep6</h1>
+    );
+}
+
+export default function Rep7() {
+    return (
+        <h1>rep7</h1>
+    );
+}
+
+export default function Rep8() {
+    return (
+        <h1>rep8</h1>
+    );
+}
+
+const Rep9 = () => {
+    return (
+        <h1>rep9</h1>
+    );
+}
+
+export {Rep9};
+
+export default function Rep10() {
+    return (
+        <h1>rep10</h1>
+    );
+}
