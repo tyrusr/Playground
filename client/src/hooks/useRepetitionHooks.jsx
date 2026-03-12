b83782f3beeda95027ef165744cb65943ee4f504
tyrusr <tyrusr17@yahoo.com>
2026-03-12 12:56:02 -0700

    did some reps in repitition hook

--- client/src/hooks/useRepetitionHooks.jsx
+++ client/src/hooks/useRepetitionHooks.jsx
@@ -1,3 +1,27 @@
 import { useEffect, useState } from "react";
 // for getting multiple practice reps in
 
+export default function useRep1() {
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState([]);
+
+
+    useEffect(() => {
+        async function getData(params){
+            setLoading(true);
+            try {
+                const response = "fetch response";
+                setData(response);
+            } catch(err) {
+                setError(err);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        getData();
+    }, []);
+
+    return {data, error, loading}
+}
