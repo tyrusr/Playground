7c1ecb6a5f76540736df3d0f54eddfb039919dbc
tyrusr <tyrusr17@yahoo.com>
2026-03-20 16:07:48 -0700

    did a rep in repitition controller

--- server/controllers/repititionController.js
+++ server/controllers/repititionController.js
@@ -1,2 +1,16 @@
 // for getting multiple practice reps in
 
+export default async function Example1(req,res,next) {
+    const request = req.body();
+
+    try {
+        //logic goess here
+        const sample = await sample();
+
+        if (!sample.ok) {return res.status(404).json("yes");}
+        
+        return res.status(200).json({"yes"});
+    } catch(err) {
+        next(err);
+    }
+}
