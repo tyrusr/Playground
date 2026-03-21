aea8e6efaef46dd4ff1f19a70a60072bf7981418
tyrusr <tyrusr17@yahoo.com>
2026-03-21 11:27:07 -0700

    did some reps in repitition controller

--- server/controllers/repititionController.js
+++ server/controllers/repititionController.js
@@ -14,3 +14,28 @@ export default async function Example1(req,res,next) {
         next(err);
     }
 }
+
+export async function Exapmle2(req, res, next) {
+    const request = req.body.field();
+
+    try {
+        //logic goes here
+        const dbcall = "get from db";
+
+        return res.status(200).json({dbcall});
+    } catch(err) {
+        next(err);
+    }
+}
+
+const Example3 = async (req,res,next) => {
+    const request = req.body.field();
+
+    try {
+        //logic goes here
+
+        return res.status(200)
+    } catch(err) {
+        next(err);
+    }
+}
