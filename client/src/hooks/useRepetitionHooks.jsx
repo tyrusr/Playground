575e99522cca2e60116a29ac1a8ef914a218cfdf
tyrusr <tyrusr17@yahoo.com>
2026-03-30 22:45:26 -0700

    did soem reps in repitition hook

--- client/src/hooks/useRepetitionHooks.jsx
+++ client/src/hooks/useRepetitionHooks.jsx
@@ -1,3 +1,24 @@
 import { useEffect, useState } from "react";
 // for getting multiple practice reps in
 
+export async function getData(params) {
+    const csrfToken = "get from cookies";
+
+    const response = fetch('/', {
+        method:"POST",
+        headers: {
+            "Content-Type": "application/json",
+            "X-CSRF-Token": csrfToken,
+        },
+        credentials:"include",
+
+        body: JSON.stringify(params);
+    })
+
+    if (!response.ok){
+        throw new Error("response not ok");
+    }
+
+    const res = response.json();
+    return res;
+}
