5f24e43169547fd07cffeaee1d56c563e1a888ae
tyrusr <tyrusr17@yahoo.com>
2026-04-01 23:34:55 -0700

    did some reps in repitition component

--- client/src/components/RepetitionComponent.jsx
+++ client/src/components/RepetitionComponent.jsx
@@ -1,2 +1,29 @@
 // for getting multiple practice reps in
 
+const rep1 = () => {
+    //logic goes here
+
+    return (
+        <h1>rep1</h1>
+    );
+};
+
+export default { rep1 };
+
+export function rep2(){
+    //event handler to handle button press
+        //maybe request something by a specific id
+
+    return (
+        <main> 
+            <section>rep2</section>
+            <button onClick={sample}>Push me</button>
+        </main>
+    )
+}
+
+export default function rep3(){
+    return (
+        <h1>rep3</h1>
+    );
+};
