0a9dfe987daaccf989b29a710982d5ecddcf6d82
tyrusr <tyrusr17@yahoo.com>
2026-04-06 14:41:26 -0700

    cleaned up repitition hook

--- client/src/hooks/useRepetitionHooks.jsx
+++ client/src/hooks/useRepetitionHooks.jsx
@@ -1,28 +1,4 @@
 import { useEffect, useState } from "react";
 // for getting multiple practice reps in
 
-const useRep = () => {
-    const [data, setData] = useState(null);
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        async function getData() {
-            setLoading(true);
-            try {
-                const res = await sampleServicesCall();
-
-                setData(res);
-            } catch(err) {
-                setError(err);
-            } finally {
-                setLoading(false);
-            }
-        }
-
-        getData();
-    }, []);
-
-    return { data, error, loading }
-    
-}
