59045aa10116d83d1a4cbb1f6ee5ceaea640575e
tyrusr <tyrusr17@yahoo.com>
2026-05-29 03:06:10 -0700

    chore(congif): added AGENTS.md for windsurf AI rules

--- client/src/services/practiceServices.jsx
+++ client/src/services/practiceServices.jsx
@@ -1,4 +1,4 @@
-export default async function sendRequest() {
+export async function sendRequest() {
     try {
         const response = await fetch('./ping', {
             method:"GET",
@@ -12,5 +12,23 @@ export default async function sendRequest() {
         return response.json();
     } catch (err) {
         throw err;
+    }
+}
+
+
+export async function ping() {
+    try {
+        const response = await fetch('', {
+            method:"GET:",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        })
+
+        if (!response.ok) throw new Error("response was not ok");
+
+        return response.json();
+    } catch (err) {
+        throw err;      
     }
 }
