f6c4bc5be53c7599c450165ef82fd906dcca478b
tyrusr <tyrusr17@yahoo.com>
2026-06-16 16:49:44 -0700

    did some reps in repitition component

--- client/src/components/RepetitionComponent.jsx
+++ client/src/components/RepetitionComponent.jsx
@@ -1,3 +1,22 @@
 // for getting multiple practice reps in
 
+export default function rep1(params){
 
+    return (
+        <h1>Hello world</h1>
+    );
+};
+
+export function rep2(){
+    return (
+        <h1>rep2</h1>
+    );
+};
+
+const rep3 = () => {
+    return (
+        <h1>rep3</h1>
+    );
+};
+
+export { rep3 }; 
