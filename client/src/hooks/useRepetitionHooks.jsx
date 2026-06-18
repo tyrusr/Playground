c8ce99d8d6374c2386881b7dd1f5c46c5b757809
tyrusr <tyrusr17@yahoo.com>
2026-06-18 13:52:49 -0700

    did some reps in userepitionhooks

--- client/src/hooks/useRepetitionHooks.jsx
+++ client/src/hooks/useRepetitionHooks.jsx
@@ -2,27 +2,74 @@ import { useEffect, useState } from "react";
 // for getting multiple practice reps in
 
 
+export default function useRep1(params) {
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
 
-export default function useRep1(params){
-    [data, setData] = useState(null);
-    [error, setError] = useState(null);
-    [loading, setLoading] = useState(false);
+    useEffect(() => {
 
-    useEffect(()=> {
-        async function getData(params){
+        async function getData(params){ 
             setLoading(true);
-            try {
-                const res = await sampleServices();
+            try{
+                const res = await sampleServicesCall(params);
                 setData(res);
+                
             } catch(err) {
                 setError(err);
             } finally {
-                setLoading(false);
+                setLoading(False);
             }
         }
 
-        getData();
-    }, []);
+        
+        getData(params)
+        
+    },[]);
 
-    return { data, error, loading };
+    return { data, error, loading }
 }
+
+
+
+export default function useRep2(params) {
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    async function getData(params){
+        setLoading(true);
+        try {
+            const res = await sampleServicesCall(params);
+            setData(res);
+        } catch(err) {
+            setError(err);
+        } finally {
+            setLoading(false);
+        }
+    }
+    
+    return { data, error, loading, getData }
+}
+
+const useRep3 = () => {
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    async function getData(params){
+        setLoading(true);
+        try {
+            const res = await sampleServicesCall(params);
+            setData(res);
+        } catch(err) {
+            setError(err);
+        } finally {
+            setLoading(false);
+        }
+    }
+    
+    return { data, error, loading, getData }
+}
+
+export default {useRep3};
