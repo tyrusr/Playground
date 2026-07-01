c863bb79a3592a7c8aaec76e97acf1e65c1a49b6
tyrusr <tyrusr17@yahoo.com>
2026-07-01 00:33:55 -0700

    feat:did some more reps in repitition services

--- client/src/services/repititionServices.jsx
+++ client/src/services/repititionServices.jsx
@@ -1,2 +1,29 @@
 // for getting multiple practice reps in
 
+export default function rep1(params){
+    const csrfToken = "csrftoken goes here";
+
+    try {
+        const res = await fetch('/route', {
+        method:'POST',
+
+        header:{
+            "Content-Type":"Application-Json",
+            "X-CSRF-Token": csrfToken, 
+        },
+        credentials:"include",
+
+        body: JSON.stringify(params)
+        });
+
+        if (!res) {
+            throw new Error("error with response");
+        }
+
+        return await res.json();
+    } catch (err) {
+        throw new Error("error");
+    }
+
+    
+}
